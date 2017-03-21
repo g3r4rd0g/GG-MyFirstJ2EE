3bb7edc64f3e6354af787685872db2881861eb23
root <root@udeploy.release-and-deploy-learning-circle.com>
2017-03-20 21:51:06 -0500

    yet another release

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -12,47 +12,47 @@ import javax.servlet.http.HttpServletResponse;
 import java.sql.*;
 
 /**
- * Servlet implementation class SimpleServlet
+ * Servlet implementation class SimpleServlet ...
  */
 @WebServlet("/SimpleServlet")
 public class SimpleServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    // JDBC driver name and database URL (Database credentials)
+    // JDBC driver name and database URL (Database credentials) ...
     static final String JDBC_URL = "jdbc:mysql://us-cdbr-iron-east-03.cleardb.net/ad_c018134e2c606c8?user=bd1c5726168b9c&password=02451f6c";
     
     /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response) ...
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	StringBuilder msgPage = new StringBuilder();
-    	msgPage.append("<br> STEP 1: import required packages");
+    	msgPage.append("<br> STEP 1 : import required packages");
     	
     	Connection conn = null;
     	Statement stmt = null;
     	String sql = "";
     	try{
-    		msgPage.append("<br> STEP 2: Register JDBC driver");
+    		msgPage.append("<br> STEP 2 : Register the JDBC driver");
     		Class.forName("com.mysql.jdbc.Driver");
     		
-    		msgPage.append("<br> STEP 3: Open a connection");
+    		msgPage.append("<br> STEP 3 : Open a connection");
     		conn = DriverManager.getConnection(JDBC_URL);
     		stmt = conn.createStatement();
     		
-    		msgPage.append("<br> STEP 4: Create a new table USER");
-    		sql = "CREATE TABLE User (name VARCHAR(20), age INTEGER)";
-    		stmt.execute(sql);
+//    		msgPage.append("<br> STEP 4 : Create a new table USER");
+//    		sql = "CREATE TABLE User (name VARCHAR(20), age INTEGER)";
+//   		stmt.execute(sql);
     		
-    		msgPage.append("<br> STEP 5: Filling the fields records");
+    		msgPage.append("<br> STEP 5 : Filling the fields records");
     		sql = "INSERT INTO User VALUES(\'Rapha Hespanhol\', 30)";
     		stmt.executeUpdate(sql);
     		
-    		msgPage.append("<br> STEP 6: Executing a query");
+    		msgPage.append("<br> STEP 6 : Executing a query");
     		sql = "SELECT name, age FROM User";
     		ResultSet rs = stmt.executeQuery(sql);
     		
-    		msgPage.append("<br> STEP 7: Extract data from result set");
+    		msgPage.append("<br> STEP 7 : Extract data from result set");
     		while (rs.next()){
     			// Retrieve by column name
     			msgPage.append("<br> Name: "+rs.getString("name"));
@@ -60,15 +60,15 @@ public class SimpleServlet extends HttpServlet {
     		}
     		rs.close();
     		
-    		msgPage.append("<br> STEP 8: Deleting records");
-    		sql = "DELETE FROM User WHERE name=(\'Rapha Hespanhol\'";
+    		msgPage.append("<br> STEP 8 : Deleting the records");
+    		sql = "DELETE FROM User WHERE name=\'Rapha Hespanhol\'";
     		stmt.executeUpdate(sql);
     		
-    		msgPage.append("<br> STEP 9: Removing table from database");
+    		msgPage.append("<br> STEP 9 : Removing table from database");
     		sql = "DROP TABLE User";
     		stmt.executeUpdate(sql);
     		
-    		msgPage.append("<br> STEP 10: clean up environment");
+    		msgPage.append("<br> STEP 10 : clean up the environment");
     		stmt.close();
     		conn.close();
     	} catch (SQLException se) {
@@ -92,7 +92,7 @@ public class SimpleServlet extends HttpServlet {
     		} // end finally try
     	} // end try
     	
-    	msgPage.append("<br> STEP 11: Sending steps to HTML");
+    	msgPage.append("<br> STEP 11 : Sending steps to HTML ... yowza!");
         response.setContentType("text/html");
         response.getWriter().print(msgPage);
     }
